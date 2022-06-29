205a27c91c6bf54d8a761ca5292f0eff14aa9bda
Bdotwall <brwall92@gmail.com>
2022-06-29 16:16:32 -0400

    updates

--- Jenkinsfile/practice.groovy
+++ Jenkinsfile/practice.groovy
@@ -1,39 +1,10 @@
 pipeline {
-  agent any
-  stages {
-
-    stage('Build') {
-      steps {
-         echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
-      }
-    }
-
-    stage('Test') {
-      steps {
-        script {
-          echo 'If you see this, test successful'
-        }
-      }
-    }
-
-  }
-
-post {
-        always {
-            echo 'One way or another, I have finished'
-            deleteDir() /* clean up our workspace */
-        }
-        success {
-            echo 'I succeeded!'
-        }
-        unstable {
-            echo 'I am unstable :/'
-        }
-        failure {
-            echo 'I failed :('
-        }
-        changed {
-            echo 'Things were different before...'
+    agent { docker { image 'maven:3.8.4-openjdk-11-slim' } }
+    stages {
+        stage('build') {
+            steps {
+                sh 'mvn --version'
+            }
         }
     }
 }
