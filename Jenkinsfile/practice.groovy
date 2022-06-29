cd830c6254ba4f3ec2d25ed70c83910b9808aff7
Bdotwall <brwall92@gmail.com>
2022-06-29 14:13:51 -0400

    post added

--- Jenkinsfile/practice.groovy
+++ Jenkinsfile/practice.groovy
@@ -1,22 +1,30 @@
 pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                sh './gradlew build'
-            }
+  agent any
+  stages {
+
+    stage('Build') {
+      steps {
+        script {
+          echo 'Hello World!'
         }
-        stage('Test') {
-            steps {
-                sh './gradlew check'
-            }
-        }
+      }
     }
 
-    post {
-        always {
-            archiveArtifacts artifacts: 'build/libs/**/*.jar', fingerprint: true
-            junit 'build/reports/**/*.xml'
+    stage('Test') {
+      steps {
+        script {
+          echo 'If you see this, test successful'
         }
+      }
     }
+
+  }
+
+  post {
+    sucess {
+        mail to: 'brwall92@gmail.com',
+             subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+             body: "it worked ${env.BUILD_URL}"
+    }
+}
 }
